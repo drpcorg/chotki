05c2f9b4f870a88b42321d1ca97bb43d6b2aa804
Victor Grishchenko <gritzko@pm.me>
2024-03-31 10:37:57 +0700

    Z two-way counters

--- rdx/NZ.go
+++ rdx/NZ.go
@@ -5,7 +5,7 @@ import (
 	"github.com/learn-decentralized-systems/toytlv"
 )
 
-// I is a last-write-wins int64
+// N is an increment-only uint64 counter
 
 // produce a text form (for REPL mostly)
 func Nstring(tlv []byte) (txt string) {
@@ -75,31 +75,139 @@ type NIterator struct {
 	inc uint64
 }
 
-func (ni *NIterator) Next() bool {
-	if len(ni.tlv) == 0 {
+func (a *NIterator) Next() bool {
+	if len(a.tlv) == 0 {
 		return false
 	}
-	_, hlen, blen := toytlv.ProbeHeader(ni.tlv)
+	_, hlen, blen := toytlv.ProbeHeader(a.tlv)
 	rlen := hlen + blen
-	ni.inc, ni.src = UnzipUint64Pair(ni.tlv[hlen:rlen])
-	ni.one = ni.tlv[:rlen]
-	ni.tlv = ni.tlv[rlen:]
+	a.inc, a.src = UnzipUint64Pair(a.tlv[hlen:rlen])
+	a.one = a.tlv[:rlen]
+	a.tlv = a.tlv[rlen:]
 	return true
 }
 
-func (ni *NIterator) Merge(b SortedIterator) int {
+func (a *NIterator) Merge(b SortedIterator) int {
 	bb := b.(*NIterator)
-	ac := ni.src
-	bc := bb.src
-	if ac == bc {
-		return MergeA
-	} else if ac < bc {
+	if a.src == bb.src {
+		if a.inc < bb.inc {
+			return MergeB
+		} else {
+			return MergeA
+		}
+	} else if a.src < bb.src {
 		return MergeAB
 	} else {
 		return MergeBA
 	}
 }
 
-func (ni *NIterator) Value() []byte {
-	return ni.one
+func (a *NIterator) Value() []byte {
+	return a.one
+}
+
+// Z is a two-way int64 counter
+
+// produce a text form (for REPL mostly)
+func Zstring(tlv []byte) (txt string) {
+	return fmt.Sprintf("%d", Znative(tlv))
+}
+
+// parse a text form into a TLV value
+func Zparse(txt string) (tlv []byte) {
+	var i int64
+	_, _ = fmt.Sscanf(txt, "%d", &i)
+	return Ztlv(i)
+}
+
+// convert native golang value into a TLV form
+func Ztlv(i int64) (tlv []byte) {
+	return toytlv.Record('I',
+		toytlv.TinyRecord('T', ZipIntUint64Pair(0, 0)),
+		ZipInt64(i),
+	)
+}
+
+// convert a TLV value to a native golang value
+func Znative(tlv []byte) (sum int64) {
+	rest := tlv
+	for len(rest) > 0 {
+		var one []byte
+		one, rest = toytlv.Take('I', rest)
+		_, body := toytlv.Take('T', one)
+		inc := UnzipInt64(body)
+		sum += inc
+	}
+	return
+}
+
+// merge TLV values
+func Zmerge(tlvs [][]byte) (merged []byte) {
+	ih := ItHeap[*ZIterator]{}
+	for _, tlv := range tlvs {
+		ih.Push(&ZIterator{tlv: tlv})
+	}
+	for ih.Len() > 0 {
+		merged = append(merged, ih.Next()...)
+	}
+	return
+}
+
+// produce an op that turns the old value into the new one
+func Zdelta(tlv []byte, new_val int64) (tlv_delta []byte) {
+	sum := Znative(tlv)
+	if new_val == sum {
+		return []byte{}
+	}
+	return Ztlv(new_val - sum)
+}
+
+// checks a TLV value for validity (format violations)
+func Zvalid(tlv []byte) bool {
+	return true //todo
+}
+
+func Zdiff(tlv []byte, vvdiff VV) []byte {
+	return nil //fixme
+}
+
+type ZIterator struct {
+	one []byte
+	tlv []byte
+	src uint64
+	rev int64
+	inc int64
+}
+
+func (a *ZIterator) Next() bool {
+	if len(a.tlv) == 0 {
+		return false
+	}
+	_, hlen, blen := toytlv.ProbeHeader(a.tlv)
+	rlen := hlen + blen
+	var val []byte
+	a.rev, a.src, val = FirstParse(a.tlv[hlen:rlen])
+	a.inc = UnzipInt64(val)
+	a.one = a.tlv[:rlen]
+	a.tlv = a.tlv[rlen:]
+	return true
+}
+
+func (a *ZIterator) Merge(b SortedIterator) int {
+	bb := b.(*ZIterator)
+	if a.src == bb.src {
+		if a.rev < bb.rev { // todo rev < 0
+			return MergeB
+		} else {
+			return MergeA
+		}
+	} else if a.src < bb.src {
+		return MergeAB
+	} else {
+		return MergeBA
+	}
+}
+
+func (a *ZIterator) Value() []byte {
+	return a.one
 }
--- rdx/NZ_test.go
+++ rdx/NZ_test.go
@@ -25,20 +25,66 @@ func TestNmerge(t *testing.T) {
 	)
 	assert.Equal(t, uint64(6), Nnative(one))
 	two := toytlv.Concat(
-		toytlv.Record('U', ZipUint64Pair(2, 2)),
+		toytlv.Record('U', ZipUint64Pair(3, 2)),
 		toytlv.Record('U', ZipUint64Pair(3, 3)),
 		toytlv.Record('U', ZipUint64Pair(4, 4)),
 	)
-	assert.Equal(t, uint64(9), Nnative(two))
+	assert.Equal(t, uint64(10), Nnative(two))
 
 	three := Nmerge([][]byte{one, two})
 
 	correct := toytlv.Concat(
 		toytlv.Record('U', ZipUint64Pair(1, 1)),
-		toytlv.Record('U', ZipUint64Pair(2, 2)),
+		toytlv.Record('U', ZipUint64Pair(3, 2)),
 		toytlv.Record('U', ZipUint64Pair(3, 3)),
 		toytlv.Record('U', ZipUint64Pair(4, 4)),
 	)
+
+	assert.Equal(t, correct, three)
+
+}
+
+func Itlv3(rev int64, src uint64, inc int64) []byte {
+	return toytlv.Record('I',
+		toytlv.TinyRecord('T', ZipIntUint64Pair(rev, src)),
+		ZipInt64(inc),
+	)
+}
+
+func TestZtlv(t *testing.T) {
+	fact := Ztlv(123)
+	correct := Itlv3(0, 0, 123)
+	assert.Equal(t, correct, fact)
+	str := Zstring(fact)
+	assert.Equal(t, "123", str)
+	inc := Zdelta(fact, 124)
+	assert.Equal(t, int64(1), Znative(inc))
+	// todo diff
+}
+
+func TestZmerge(t *testing.T) {
+	one := toytlv.Concat(
+		Itlv3(1, 1, 1),
+		Itlv3(2, 2, 2),
+		Itlv3(3, 3, 3),
+	)
+	assert.Equal(t, int64(6), Znative(one))
+	two := toytlv.Concat(
+		Itlv3(3, 2, 3),
+		Itlv3(3, 3, 3),
+		Itlv3(4, 4, 4),
+	)
+	assert.Equal(t, int64(10), Znative(two))
+
+	three := Zmerge([][]byte{one, two})
+
+	correct := toytlv.Concat(
+		Itlv3(1, 1, 1),
+		Itlv3(3, 2, 3),
+		Itlv3(3, 3, 3),
+		Itlv3(4, 4, 4),
+	)
+	assert.Equal(t, int64(11), Znative(correct))
 
 	assert.Equal(t, correct, three)
 
